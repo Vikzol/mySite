b92438b063e662be35c9eccab045443280d3f419
Vikzol <jalia90@mail.ru>
2026-07-27 15:37:51 +0300

    создала первую карточку и подвал

--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,18 @@
 <?php
+declare(strict_types=1);
+
+function ob_include() : string
+{
+    extract(func_get_arg(1));
+    ob_start();
+    require(func_get_arg(0));
+    return ob_get_clean();
+}
+$product_1= [
+    'name' => 'L\'Oreal Шампунь для плотности длинных волос, 500 мл',
+    'price' => '700', //7.00 руб
+    'desc' => 'Шампунь разработан специально для тех, кто мечтает о красивых 
+    и длинных волосах.'
+];
+$html_1 = ob_include('product.phtml', ['p' => $product_1]);
+echo ob_include('layout.phtml', ['content' => $html_1]);
